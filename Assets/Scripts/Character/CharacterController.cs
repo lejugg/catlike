db6ffb154c35cce1bb49e82d36d0d7d9ea61e2aa
Michele Krueger <michele.krueger@etermax.de>
2022-05-02 22:13:14 +0200

    Vehicle logic for character

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private KeyboardInput keyboardInput;
-    [SerializeField] private Rigidbody2D rigidbody;
-    [SerializeField] private float movementForce = 10f;
-    [SerializeField] private float jumpForce = 100f;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
     
-    
-    private Vector3 movement = Vector3.zero;
+    [SerializeField] private float movementForce = 1f;
+    [SerializeField] private float maxAcc = 1f;
+    [SerializeField] private float maxVel = 1f;
+    [SerializeField] private float accDrag = 0.5f;
+    [SerializeField] private float velDrag = 0.5f;
+    [SerializeField] private Vector3 gravity = new Vector3(0f, -0.9f, 0f);
+
+    private Vector3 input = Vector3.zero;
+    private Vector3 acc = Vector3.zero;
+    private Vector3 vel = Vector3.zero;
+    private Vector3 pos = Vector3.zero;
+    private Vector3 _previousPos = Vector3.zero;
+
     private bool isGrounded;
 
     private void Awake()
     {
         keyboardInput.Up += Jump;
+        keyboardInput.Down += () => { };
         
         keyboardInput.Left += () => Move(Vector3.left);
         keyboardInput.Right += () => Move(Vector3.right);
@@ -23,61 +31,88 @@ public class CharacterController : MonoBehaviour
 
     private void Move(Vector3 dir)
     {
-        if (isGrounded)
-        {
-            movement += dir;
-        } else
-        {
-            movement += dir;
-            movement *= 0.3f;
-        }
+        input += dir;
     }
 
     private void Jump()
     {
-        if (isGrounded)
-        {
-            movement += Vector3.up * jumpForce;
-        }
+        input += Vector3.up;
     }
 
     void FixedUpdate()
     {
-        CheckGround();
+        _previousPos = transform.position;
 
-        ApplyMovementForce();
-    }
-
-    private void ApplyMovementForce()
-    {
-        movement *= movementForce;
-        rigidbody.AddForce(movement);
-        movement = Vector3.zero;
-    }
-
-    private void CheckGround()
-    {
-        var hit = Physics2D.CircleCast(transform.position , 0.4f, UnityEngine.Vector2.down, 0.21f);
+        CheckIsGrounded();
+        ApplyMovement();
         
-        if (hit.collider != null ) 
+        Debug.DrawLine(_previousPos, transform.position, Color.green, 5f);
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = transform.position.y < 0;
+    }
+
+    private void ApplyMovement()
+    {
+        input.Normalize();
+        input *= movementForce;
+
+        acc += input;
+        Vector3.ClampMagnitude(acc, maxAcc);
+        if(!isGrounded) acc += gravity;
+
+        vel += acc;
+        Vector3.ClampMagnitude(vel, maxVel);
+
+        pos += vel;
+        transform.position = pos;
+
+        if (input.magnitude < maxAcc)
         {
-            Debug.DrawLine(transform.position, hit.point, Color.green, 10f);
-            isGrounded = true;
-        } else
+            input = Vector3.zero;
+        }
+        else
         {
-            isGrounded = false;
+            input *= (input.magnitude - maxAcc);
         }
 
-        _spriteRenderer.color = isGrounded ? Color.white : Color.green;
+        acc *= accDrag;
+        vel *= velDrag;
     }
 
-    public Vector2 GetVelocity()
-    {
-        return rigidbody.velocity;
-    }
+    // private void ApplyMovementForce()
+    // {
+    //     input *= movementForce;
+    //     rigidbody.AddForce(input);
+    //     input = Vector3.zero;
+    // }
+    //
+    // private void CheckGround()
+    // {
+    //     var hit = Physics2D.CircleCast(transform.position , 0.4f, UnityEngine.Vector2.down, 0.21f);
+    //     
+    //     if (hit.collider != null ) 
+    //     {
+    //         Debug.DrawLine(transform.position, hit.point, Color.green, 10f);
+    //         isGrounded = true;
+    //     } else
+    //     {
+    //         isGrounded = false;
+    //     }
+    //
+    //     _spriteRenderer.color = isGrounded ? Color.white : Color.green;
+    // }
+
 
     public bool GetIsGrounded()
     {
         return isGrounded;
+    }
+
+    public Vector2 GetVelocity()
+    {
+        return Vector2.zero;
     }
 }
